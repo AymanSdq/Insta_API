6000bf989d8dff1dd661fad4b7b142740e95c788
Ayman Sedqi <sedqiayman02@gmail.com>
2024-02-12 18:23:53 +0100

    Adding the edited sections

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -3,15 +3,37 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const authenticateToken = require("../middlewares/authToken");
 const Register = require("../models/Register");
+const { route } = require("./loginRoutes");
 
 
-router.get("/", authenticateToken , (req, res) => {
-
-    res.json({user : req.user});
+router.get("/", authenticateToken , async (req, res) => {
 
 
-    
-})
+    const findUserAuth = await Register.findById(req.user.id)
+    res.send(findUserAuth);
+
+});
+
+router.patch("/:id", authenticateToken, async (req , res) => {
+
+    const {username, fullName, email, password} = req.body;
+
+    // Save the data in Objects
+    const updates = {};
+    // Check if there is update
+    if(username){
+        updates.username = username;
+    }
+    if(fullName){
+        updates.username = username;
+    }
+    if(email){
+        updates.username = username;
+    }
+
+
+
+});
 
 
 module.exports = router;
