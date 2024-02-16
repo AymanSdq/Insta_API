f2b5ee77b61d0f45ce8a7130aef6ece3bef0387e
Ayman Sedqi <sedqiayman02@gmail.com>
2024-02-17 00:01:46 +0100

    Adding the POST Models and Routes

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+// Adding the schema to this variable
+const Schema = mongoose.Schema;
+
+
+// Creating the post
+
+const postSchema = new Schema({
+
+    // The post will be related to this user
+    userId : {
+        type :  Schema.Types.ObjectId,
+        ref : "Register",
+    },
+
+    postImgVid : [{
+        type : String,
+        required : true,
+
+    }],
+
+    postCaption : {
+        type : String,
+        maxlength : 150
+    },
+
+    postCreatedAt : {
+        type : Date,
+        default : Date.now
+    },
+
+    postUpdatedAt : {
+        type : Date,
+        default : Date.now
+    },
+
+
+
+    // Must add the like and the comments but do it later because itself needes a Models to create
+    
+
+    
+});
---
+++ routes/createPost.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const router = express.Router();
+const authenticateToken = require("../middlewares/authToken");
+
+
+app.use(express.json());
+
+
+router.post("/" , authenticateToken , (req, res) => {
+
+    // Posting the post on instagram routes
+
+
+});
